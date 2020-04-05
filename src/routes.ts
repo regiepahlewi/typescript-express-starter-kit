11bdda0c9c5830b3f49d19a2785e9317b5452a22
Regie Pahlewi <regiealgipahlewi@gmail.com>
2020-04-05 12:39:59 +0700

    add get all route

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,12 @@ import { RegistrationController } from "./controller/registration.controller";
 
 export const Routes = [
     {
+        method: "get",
+        route: "/registration",
+        controller: RegistrationController,
+        action: "all"
+    },
+    {
         method: "post",
         route: "/registration",
         controller: RegistrationController,
