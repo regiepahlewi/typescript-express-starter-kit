b385b25a0acac0abbf71dcc9aac3942b6116ffa5
Regie Pahlewi <regiealgipahlewi@gmail.com>
2020-04-05 11:05:14 +0700

    add new constanst

--- src/constants/string.constants.ts
+++ src/constants/string.constants.ts
@@ -1,3 +1,4 @@
 export class StringConstants {
     static MSG_SUCCESS_INSERT = 'Data has been added successfully';
+    static MSG_ERROR_500 = 'Internal Server Error, Please contact your administrator.';
 }
