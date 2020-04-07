bc3db62ebeb47fb32c9a24245bf84661bbb83f1c
Regie Pahlewi <regiealgipahlewi@gmail.com>
2020-04-07 10:44:30 +0700

    remove req.path

--- src/middleware/Logger.ts
+++ src/middleware/Logger.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 
 const loggerMiddleware = (req: Request, resp: Response, next) => {
 
-    console.log('Request logged:', req.method, req.path, resp.statusCode)
+    console.log('Request logged:', req.method, req.path)
     next()
 }
 
