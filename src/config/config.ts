5195a009805d79e789fde3732182d42c5ae92ae7
Regie Pahlewi <regiealgipahlewi@gmail.com>
2020-04-18 01:03:59 +0700

    integrate config with dotenv

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,20 @@
+import * as dotenv from "dotenv";
 export class Config {
+
+    private static getCommonEnvironment() {
+        dotenv.config();
+
+        const config = {
+            DATE_FORMAT: process.env.DATE_FORMAT,
+            JWT_SECRET: process.env.JWT_SECRET
+        }
+
+        return config
+    }
+
     // Date Format
-    static DATE_FORMAT = 'YYYY-MM-DD';
+    static DATE_FORMAT = Config.getCommonEnvironment().DATE_FORMAT;
 
     // JWT
-    static JWT_SECRET = "#re3gi3@lg1##";
-    
+    static JWT_SECRET = Config.getCommonEnvironment().JWT_SECRET;
 };
